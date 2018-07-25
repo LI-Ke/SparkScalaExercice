74fda5d772486d85aa36393c0db82e207c37b772
Ke LI <ke.li1@etu.upmc.fr>
2018-07-25 17:39:21 +0200

    Modify Flatmap.scala and add Timestamp.scala

--- build.sbt
+++ build.sbt
@@ -4,9 +4,9 @@ version := "0.1"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.1"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.3.1"
 
 
 mainClass in assembly := some("spark.basic.Flatmap")
--- src/main/scala/spark/basic/Flatmap.scala
+++ src/main/scala/spark/basic/Flatmap.scala
@@ -4,10 +4,16 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object Flatmap {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("flatmap").setMaster("local[1]")
+//    val conf = new SparkConf().setAppName("flatmap").setMaster("spark://small1.common.lip6.fr:7077")
+    val conf = new SparkConf().setAppName("flatmap").setMaster("local[*]")
+/*    conf.set("spark.sql.shuffle.partitions", "130")
+    conf.set("spark.cores.max", "130")
+    conf.set("spark.executor.memory", "30G")*/
+
     val sc = new SparkContext(conf)
-    val arr=sc.parallelize(Array(("A",1),("B",2),("C",3)))
+    val arr=sc.parallelize(Array(("A",1),("B",2),("Z",3)))
     arr.flatMap(x => x._1 + x._2).foreach(println)
+    sc.stop()
   }
 
 }
